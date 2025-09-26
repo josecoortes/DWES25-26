8a43b0c25fdd8ca55b6a4f7c924e9a3fddc97d81
josecoortes <jcormar957@g.educaand.es>
2025-09-26 17:55:01 +0200

    act2_4 completado

---
+++ www/Unidad2/act2/act2_4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Act 4</title>
+
+    <style>
+        body {
+            font-family: 'Libre Baskerville', serif;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $saldo = "1000";
+    $ingreso = 250;
+    settype($saldo, "integer");
+    $nomina = $saldo + $ingreso; 
+    print $nomina > 1200 ? 'Cliente VIP' : 'Cliente normal';
+    ?>
+</body>
+</html>
