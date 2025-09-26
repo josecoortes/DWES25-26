86467f4d48a8fc4eecb5240453d828d920a8ccaa
josecoortes <jcormar957@g.educaand.es>
2025-09-26 17:43:43 +0200

    act3 subida

---
+++ www/Unidad2/act2/act2_3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Act 3</title>
+
+    <style>
+        body {
+            font-family: 'Libre Baskerville', serif;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $a = 1;
+    echo ($a <=> 0) == 0 ? 'cero' : (($a <=> 0) == 1 ? 'positivo' : 'negativo');
+    ?>
+</body>
+</html>
