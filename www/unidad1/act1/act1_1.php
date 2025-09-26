76e3fe6fcc3ecdb9fd49e4b7438611bc5d1dd8bb
josecoortes <jcormar957@g.educaand.es>
2025-09-26 16:23:59 +0200

    arreglo estrcutura proyecto

---
+++ www/unidad1/act1/act1_1.php
@@ -0,0 +1,23 @@
+<?php
+/*
+---
+title: Ejercicio 1: Primer ejercicio en PHP
+desc: Hola Mundo en PHP
+tags: [PHP, basico]
+code: https://github.com/usuario/repo/blob/main/unidad1/ej2.php
+---
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hola Mundo desde PHP";
+    ?>
+</body>
+</html>
